f1193d3ec16d4eb7f573857e3b2e3ea6ffcaafab
mdYeasinIslam <hyeasinislam@gmail.com>
2026-05-14 15:05:19 +0600

    fix mongodb connection related issue: deployment related

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,11 @@ import http from "http";
 import app from "./App";
 import connectDB from "./app/config/database";
 import handleSocket from "./app/socket/socket";
+console.log("ENV CHECK:", {
+  MONGODB_URI: process.env.MONGODB_URI ? "✅ found" : "❌ undefined",
+  PORT: process.env.PORT,
+});
+
 let server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 handleSocket(server);
