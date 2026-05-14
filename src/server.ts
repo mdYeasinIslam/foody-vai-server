a149eca74bbb11188102d952a063f022cef5afc8
mdYeasinIslam <hyeasinislam@gmail.com>
2026-05-14 15:35:35 +0600

    fix: move typescript to dependencies for Railway build

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import app from "./App";
 import connectDB from "./app/config/database";
 import handleSocket from "./app/socket/socket";
 console.log("ENV CHECK:", {
-  MONGODB_URI: process.env.MONGODB_URI ? "✅ found" : "❌ undefined",
+  MONGODB_URI: process.env.MONGODB_URI ? "✅found" : "❌ undefined",
   PORT: process.env.PORT,
 });
 
